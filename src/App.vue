7107ddc90682320ea8d05e7e6f1ecdc83a1a9220
TatianaAll <tatiana.allery@orange.fr>
2025-10-14 16:42:17 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script setup></script>
+
+<template>
+  <header>
+    <nav>
+      <ul>
+        <li>
+          <RouterLink to="/">Home</RouterLink>
+        </li>
+        <li></li>
+        <li></li>
+      </ul>
+    </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style scoped></style>
