55d2d2062c705067063258ee7fb30276f662bb2f
TatianaAll <tatiana.allery@orange.fr>
2025-10-14 17:11:36 +0200

    re-indentation

--- src/router.js
+++ src/router.js
@@ -2,8 +2,6 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import HomeView from "./pages/HomeView.vue";
 
-
-
 const routes = [{ path: "/", component: HomeView }];
 
 export const router = createRouter({
