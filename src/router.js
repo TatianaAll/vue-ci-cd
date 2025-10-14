60271a41104d53956c252d940541778503ad7bb4
TatianaAll <tatiana.allery@orange.fr>
2025-10-14 16:54:58 +0200

    test commit with linter

--- src/router.js
+++ src/router.js
@@ -2,6 +2,8 @@ import { createMemoryHistory, createRouter } from "vue-router";
 
 import HomeView from "./pages/HomeView.vue";
 
+
+
 const routes = [{ path: "/", component: HomeView }];
 
 export const router = createRouter({
