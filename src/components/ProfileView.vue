77355f9a6cc619c492593394fb10f3fa833fd4df
TatianaAll <tatiana.allery@orange.fr>
2025-10-15 09:41:58 +0200

    feat: json resume and profile view

---
+++ src/components/ProfileView.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="p-4 bg-gray-100">
+    <h2 class="text-2xl font-bold mb-4">Profile Component</h2>
+    <p>This is the profile component content.</p>
+  </div>
+</template>
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -1,3 +1,10 @@
+<script setup>
+import { ref } from "vue";
+const showHello = () => {
+  ref("Hello from HomeView");
+};
+</script>
 <template>
   <h1>Home view</h1>
+  <button @click="showHello">Click me</button>
 </template>
